45049e0d928e2ff5df0e962cad8d2639915cd77d
baichangda <471267877@qq.com>
2017-07-26 14:33:36 +0800

    c

---
+++ Base/src/main/java/com/base/define/BaseErrorDefine.java
@@ -0,0 +1,16 @@
+package com.base.define;
+
+import com.base.message.BaseErrorMessage;
+
+/**
+ * Created by Administrator on 2017/7/26.
+ */
+public class BaseErrorDefine {
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+    public final static BaseErrorMessage ERROR_GET_JPA_PK_VALUE=BaseErrorMessage.getMessage("获取JPA主键值失败!");
+}
