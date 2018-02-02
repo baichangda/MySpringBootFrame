15dd6ccee2023c091fb635eb427750a6bb79c8f0
baichangda <471267877@qq.com>
2018-02-02 15:21:42 +0800

    c

--- src/main/java/com/bcd/Application.java
+++ src/main/java/com/bcd/Application.java
@@ -1,5 +1,6 @@
 package com.bcd;
 
+import com.bcd.nettyserver.http.NettyHttpServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        new NettyHttpServer().run();
     }
 }
