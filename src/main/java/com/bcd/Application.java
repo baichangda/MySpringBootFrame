80a728389b84c67988bf4f883f49e405b8c78709
baichangda <471267877@qq.com>
2018-02-08 17:24:05 +0800

    c

--- src/main/java/com/bcd/Application.java
+++ src/main/java/com/bcd/Application.java
@@ -1,14 +1,21 @@
 package com.bcd;
 
 import com.bcd.nettyserver.http.NettyHttpServer;
+import com.bcd.nettyserver.http.listener.TimeoutListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication(scanBasePackages="com.bcd")
 public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-        new NettyHttpServer("test",10001).run();
+        Executors.newSingleThreadExecutor().execute(()->new NettyHttpServer("test",10001).run());
+        //启动netty延时任务超时监听线程(每1s扫描一次)
+        Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(new TimeoutListener(), 3000L, 1000L, TimeUnit.MILLISECONDS);
     }
 }
