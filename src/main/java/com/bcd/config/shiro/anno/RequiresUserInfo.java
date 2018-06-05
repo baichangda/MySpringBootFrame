1eee3ee2c2cc7501d43233a3afc35c49e13ac9ec
baichangda <471267877@qq.com>
2018-06-05 09:54:10 +0800

    c

---
+++ src/main/java/com/bcd/config/shiro/anno/RequiresUserInfo.java
@@ -0,0 +1,19 @@
+package com.bcd.base.annotation;
+
+import org.apache.shiro.authz.annotation.Logical;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresUserInfo {
+    long[] id() default {};
+    String[] username() default {};
+    Logical logical() default Logical.AND;
+}
