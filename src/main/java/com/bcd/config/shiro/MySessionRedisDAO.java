6a0019c68eaf46b8b7f81fe38cb3625b78065b3c
baichangda <471267877@qq.com>
2018-07-04 15:08:34 +0800

    c

--- src/main/java/com/bcd/config/shiro/MySessionRedisDAO.java
+++ src/main/java/com/bcd/config/shiro/MySessionRedisDAO.java
@@ -4,9 +4,16 @@ import com.bcd.base.util.SerializerUtil;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.lang.Nullable;
+import redis.clients.jedis.Jedis;
 
 import java.io.*;
 import java.util.concurrent.TimeUnit;
@@ -65,7 +72,13 @@ public class MySessionRedisDAO extends EnterpriseCacheSessionDAO {
     protected void doUpdate(Session session) {
         super.doUpdate(session);
         //这里更新redis用户信息过期时间
-        boolean res=redisOp.getOperations().expire(session.getId().toString().getBytes(),TIME_OUT_SECONDS, TimeUnit.SECONDS);
+        Boolean res=(Boolean)redisOp.getOperations().execute(new RedisCallback<Object>() {
+            @Nullable
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.set(session.getId().toString().getBytes(),SerializerUtil.serialize(session),Expiration.seconds(TIME_OUT_SECONDS), RedisStringCommands.SetOption.SET_IF_PRESENT);
+            }
+        });
         if(!res){
             session.stop();
         }
