fac4d0ef02cd7961e751673773d0305ef5208fed
baichangda <471267877@qq.com>
2019-04-01 09:43:48 +0800

    c

--- Base/src/main/java/com/bcd/base/websocket/client/BaseApiWebSocketClient.java
+++ Base/src/main/java/com/bcd/base/websocket/client/BaseApiWebSocketClient.java
@@ -11,7 +11,13 @@ public abstract class BaseApiWebSocketClient extends BaseJsonWebSocketClient<Api
     }
 
     /**
-     *  调用方法
+     * 阻塞调用请求
+     * @param apiName 需要调用的api名称
+     * @param params  调用api需要传入的参数
+     * @param timeOut  调用api的超时事件
+     * @param clazzs 返回参数泛型类型数组,只支持类型单泛型,例如:
+     *               JsonMessage<<WebData<VehicleBean>>> 传参数 JsonMessage.class,WebData.class,VehicleBean.class
+     * @return 返回null表示超时;其他则表示正常
      */
     public <R>WebSocketData<R> call(String apiName, Object[] params, long timeOut, Class ... clazzs){
         ApiCommand apiCommand=new ApiCommand();
