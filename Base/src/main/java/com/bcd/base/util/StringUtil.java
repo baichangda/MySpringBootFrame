3a1fa163db7ce5cdd904b7b16d284848ac258138
baichangda <471267877@qq.com>
2020-03-23 11:38:40 +0800

    c

--- Base/src/main/java/com/bcd/base/util/StringUtil.java
+++ Base/src/main/java/com/bcd/base/util/StringUtil.java
@@ -1,12 +1,16 @@
 package com.bcd.base.util;
 
 
+import com.bcd.base.exception.BaseRuntimeException;
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SuppressWarnings("unchecked")
 public class StringUtil {
+
+    static final Pattern NUMBER_PATTERN=Pattern.compile("^(\\-|\\+)?\\d+(\\.\\d+)?$");
 
     /**
      * 将一串包含特殊字符串的换成驼峰模式
@@ -144,5 +148,143 @@ public class StringUtil {
             returnList.add(matcher.group(1));
         }
         return returnList;
+    }
+
+    /**
+     * 将算数字符串转换成逆波兰表达式
+     * 算数支持 + - * / ( ) 符号
+     * @return
+     */
+    public static List<String> parseArithmeticToRPN(String str){
+        List<String> output=new ArrayList<>();
+        int stackIndex=-1;
+        char[] stack=new char[str.length()];
+        char[] arr= str.toCharArray();
+        StringBuilder temp=new StringBuilder();
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]=='+'||arr[i]=='-'||arr[i]=='*'||arr[i]=='/'){
+                if(temp.length()>0) {
+                    output.add(temp.toString());
+                    temp.delete(0, temp.length());
+                }
+                if(stackIndex>=0){
+                    while(stack[stackIndex]!='('&&get(stack[stackIndex])>=get(arr[i])){
+                        output.add(String.valueOf(stack[stackIndex--]));
+                        if(stackIndex==-1){
+                            break;
+                        }
+                    }
+                }
+                stack[++stackIndex]=arr[i];
+            }else if(arr[i]=='('){
+                stack[++stackIndex]=arr[i];
+            }else if(arr[i]==')'){
+                if(temp.length()>0) {
+                    output.add(temp.toString());
+                    temp.delete(0, temp.length());
+                }
+                while(stackIndex>=0){
+                    char c= stack[stackIndex--];
+                    if(c=='('){
+                        break;
+                    }
+                    output.add(String.valueOf(c));
+                }
+            }else{
+                temp.append(arr[i]);
+            }
+        }
+
+        if(temp.length()>0){
+            output.add(temp.toString());
+        }
+
+        for(int i=stackIndex;i>=0;i--){
+            output.add(String.valueOf(stack[i]));
+        }
+
+        return output;
+    }
+
+    /**
+     * 获取字符优先级
+     * @param c
+     * @return
+     */
+    private static int get(char c){
+        switch (c){
+            case '+':{
+                return 1;
+            }
+            case '-':{
+                return 1;
+            }
+            case '*':{
+                return 2;
+            }
+            case '/':{
+                return 2;
+            }
+            default:{
+                throw BaseRuntimeException.getException("symbol["+c+"] not support");
+            }
+        }
+    }
+
+    /**
+     * 计算逆波兰表达式
+     * @param list 逆波兰表达式集合
+     * @param map 字段和值对应map
+     * @return
+     */
+    public static Double calcRPN(List<String> list,Map<String,Double> map){
+        int stackIndex=-1;
+        Double[] stack=new Double[list.size()];
+        for (String s : list) {
+            switch (s){
+                case "+":{
+                    double num2=stack[stackIndex--];
+                    double num1=stack[stackIndex--];
+                    stack[++stackIndex]=num1+num2;
+                    break;
+                }
+                case "-":{
+                    double num2=stack[stackIndex--];
+                    double num1=stack[stackIndex--];
+                    stack[++stackIndex]=num1-num2;
+                    break;
+                }
+                case "*":{
+                    double num2=stack[stackIndex--];
+                    double num1=stack[stackIndex--];
+                    stack[++stackIndex]=num1*num2;
+                    break;
+                }
+                case "/":{
+                    double num2=stack[stackIndex--];
+                    double num1=stack[stackIndex--];
+                    stack[++stackIndex]=num1/num2;
+                    break;
+                }
+                default:{
+                    //如果是数字
+                    if(NUMBER_PATTERN.matcher(s).matches()){
+                        stack[++stackIndex]=Double.parseDouble(s);
+                    }else{
+                        Double val=map.get(s);
+                        if(val==null){
+                            throw BaseRuntimeException.getException("map val["+s+"] not exists");
+                        }
+                        stack[++stackIndex]=val;
+                    }
+                    break;
+                }
+            }
+        }
+        return stack[0];
+    }
+
+    public static void main(String[] args) {
+        List<String> res= parseArithmeticToRPN("n1");
     }
 }
