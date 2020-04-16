275ad744c8d730fdd5fc0407bb8f4dddd3530908
baichangda <471267877@qq.com>
2020-04-16 17:00:15 +0800

    c

--- Sys/src/main/resources/db/migration/V20170712_9__t_sys_org_role.sql
+++
@@ -1,9 +0,0 @@
-create table IF NOT EXISTS t_sys_org_role
-(
-   id                   bigint unsigned not null auto_increment comment 'id',
-   org_id               bigint comment '关联机构id',
-   role_code            varchar(50) comment '关联角色编码',
-   primary key (id)
-);
-
-alter table t_sys_org_role comment '角色与机构关联关系表';
