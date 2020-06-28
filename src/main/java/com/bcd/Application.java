bfbfc57b5f1b4962f8623bc520ba6313c04b434d
baichangda <471267877@qq.com>
2020-06-28 14:31:33 +0800

    c

--- src/main/java/com/bcd/Application.java
+++ src/main/java/com/bcd/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @EnableScheduling
 @SpringBootApplication(scanBasePackages= {"com.bcd"})
-public class Application {
+public class Application{
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
