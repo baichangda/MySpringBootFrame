bdf25d71287984c9b8756f262b723c4e44576304
471267877@qq.com <bcd@5221527>
2020-09-24 13:59:46 +0800

    c

--- src/main/java/com/bcd/controller/HealthCheckController.java
+++ src/main/java/com/bcd/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.bcd.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Api(tags = "健康监测/HealthCheckController")
 public class HealthCheckController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     @ApiOperation(value = "健康检查(默认访问路径)",notes = "健康检查(默认访问路径)")
