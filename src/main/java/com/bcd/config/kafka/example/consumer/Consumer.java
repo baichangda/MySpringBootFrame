0acb2a19ecbd6b5b24789183cf1ef2ba9478c7ab
471267877@qq.com <bcd@5221527>
2020-09-27 16:20:55 +0800

    c

--- src/main/java/com/bcd/config/kafka/example/consumer/Consumer.java
+++ src/main/java/com/bcd/config/kafka/example/consumer/Consumer.java
@@ -1,6 +1,8 @@
 package com.bcd.config.kafka.example.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +19,16 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
 
-//@Service
+@Service
 public class Consumer {
+    Logger logger= LoggerFactory.getLogger(Consumer.class);
     @KafkaListener(topics = "zs-feedback",groupId = "zs-device-platform1")
     public void processMessage(ConsumerRecord<byte[],byte[]> consumerRecord) {
-        System.out.println(new String(consumerRecord.value()));
+        logger.info("receive message key[{}] value[{}]",new String(consumerRecord.key()),new String(consumerRecord.value()));
+        for (org.apache.kafka.clients.consumer.Consumer consumer : ConsumerConfig.consumerList) {
+            consumer.
+
+        }
     }
 
 }
