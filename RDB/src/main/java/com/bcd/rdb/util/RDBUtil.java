6db906fcfbb77f68df65fc1509e27d89007eb008
baichangda <471267877@qq.com>
2021-01-12 16:30:41 +0800

    c

--- RDB/src/main/java/com/bcd/rdb/util/RDBUtil.java
+++ RDB/src/main/java/com/bcd/rdb/util/RDBUtil.java
@@ -14,28 +14,6 @@ import java.util.Map;
 public class RDBUtil {
 
     /**
-     * 根据字段名称生成参数名称
-     * @param fieldName
-     * @param paramToCount 参数字段和本次sql中出现的次数
-     * @return
-     */
-    public static String generateRandomParamName(String fieldName,Map<String,Integer> paramToCount){
-        String paramName;
-        if(fieldName.contains(".")){
-            paramName=fieldName.substring(fieldName.indexOf('.')+1);
-        }else{
-            paramName=fieldName;
-        }
-        Integer count=paramToCount.get(paramName);
-        if(count==null){
-            count=0;
-        }
-        paramToCount.put(paramName,count+1);
-        return paramName+"_"+ count;
-    }
-
-
-    /**
      * 获取JPA接口 实体类 泛型类型
      * @param obj
      * @return
