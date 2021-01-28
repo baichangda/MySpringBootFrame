d6cae680fc547c66fdcc199badd021b74f535dca
baichangda <471267877@qq.com>
2021-01-28 18:02:34 +0800

    c

--- Base/src/main/java/com/bcd/base/config/redis/mq/ValueSerializerType.java
+++ Base/src/main/java/com/bcd/base/config/redis/mq/ValueSerializerType.java
@@ -1,7 +1,20 @@
 package com.bcd.base.config.redis.mq;
 
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+
+import java.lang.reflect.Type;
+
 public enum ValueSerializerType {
+        /**
+         * {@link org.springframework.data.redis.serializer.StringRedisSerializer#UTF_8}
+         */
         STRING,
+        /**
+         * {@link com.bcd.base.config.redis.RedisUtil#newJackson2JsonRedisSerializer(Type)}
+         */
         JACKSON,
+        /**
+         * {@link JdkSerializationRedisSerializer#JdkSerializationRedisSerializer()}
+         */
         SERIALIZABLE
 }
