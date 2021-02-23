51cf0e959efbebbdfa5017f5c0cc482cfb554d14
baichangda <471267877@qq.com>
2021-02-23 16:59:48 +0800

    c

--- RDB/src/main/java/com/bcd/rdb/dbinfo/controller/DBController.java
+++ RDB/src/main/java/com/bcd/rdb/dbinfo/controller/DBController.java
@@ -4,10 +4,10 @@ import com.bcd.base.controller.BaseController;
 import com.bcd.base.exception.BaseRuntimeException;
 import com.bcd.base.util.I18nUtil;
 import com.bcd.rdb.dbinfo.service.TablesService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/tables")
-@Api(tags = "数据库设计/TablesController")
+@Tag(name = "TablesController",description = "数据库设计")
 public class TablesController extends BaseController {
 
     Logger logger = LoggerFactory.getLogger(TablesController.class);
@@ -31,10 +31,10 @@ public class TablesController extends BaseController {
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/exportSpringDBDesignerExcel", method = RequestMethod.GET)
-    @ApiOperation(value = "导出spring数据库设计", notes = "导出spring数据库设计")
-    @ApiResponse(code = 200, message = "导出结果")
+    @Operation(description = "导出spring数据库设计")
+    @ApiResponse(responseCode = "200",description = "导出结果")
     public void exportSpringDBDesignerExcel(
-            @ApiParam(value = "数据库名称") @RequestParam(value = "dbName", required = true) String dbName,
+            @Parameter(name = "数据库名称") @RequestParam(value = "dbName", required = true) String dbName,
             HttpServletResponse response) {
         try {
             tablesService.exportSpringDBDesignerExcel(dbName, response.getOutputStream(), () -> {
@@ -48,13 +48,13 @@ public class TablesController extends BaseController {
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/exportDBDesignerExcel", method = RequestMethod.GET)
-    @ApiOperation(value = "导出数据库设计", notes = "导出数据库设计")
-    @ApiResponse(code = 200, message = "导出结果")
+    @Operation(description = "导出数据库设计")
+    @ApiResponse(responseCode = "200", description = "导出结果")
     public void exportDBDesignerExcel(
-            @ApiParam(value = "数据库url(例如:127.0.0.1:3306)") @RequestParam(value = "url", required = true) String url,
-            @ApiParam(value = "数据库用户名") @RequestParam(value = "username", required = true) String username,
-            @ApiParam(value = "数据库密码") @RequestParam(value = "password", required = true) String password,
-            @ApiParam(value = "数据库名称") @RequestParam(value = "dbName", required = true) String dbName,
+            @Parameter(name = "数据库url(例如:127.0.0.1:3306)") @RequestParam(value = "url", required = true) String url,
+            @Parameter(name = "数据库用户名") @RequestParam(value = "username", required = true) String username,
+            @Parameter(name = "数据库密码") @RequestParam(value = "password", required = true) String password,
+            @Parameter(name = "数据库名称") @RequestParam(value = "dbName", required = true) String dbName,
             HttpServletResponse response) {
         try {
             tablesService.exportDBDesignerExcel(url, username, password, dbName, response.getOutputStream(), () -> {
