a069f6e21ce66db8403d4fdb4a516c17b959b4c7
baichangda <471267877@qq.com>
2021-05-06 16:56:07 +0800

    c

---
+++ src/main/java/com/bcd/config/openapi/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.bcd.config.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
+import org.apache.shiro.web.servlet.ShiroHttpSession;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig{
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API")
+                        .description("bcd application")
+                        .version("v0.0.1"));
+    }
+
+    /**
+     * 添加全局的请求头参数
+     */
+    @Bean
+    public OpenApiCustomiser openApiCustomiser() {
+        return openApi -> openApi.getPaths().values().stream().flatMap(pathItem -> pathItem.readOperations().stream())
+                .forEach(operation -> {
+                    operation.addParametersItem(new HeaderParameter().name(ShiroHttpSession.DEFAULT_SESSION_ID_NAME).description("shiro session id"));
+                });
+    }
+}
