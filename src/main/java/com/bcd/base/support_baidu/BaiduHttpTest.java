c7d5bcd3409def9d39cc55bf325a7fb3b1da8901
baichangda <471267877@qq.com>
2021-07-22 16:54:05 +0800

    c

--- src/main/java/com/bcd/base/support_baidu/BaiduHttpTest.java
+++
@@ -1,9 +0,0 @@
-package com.bcd.base.support_baidu;
-
-import java.io.IOException;
-
-public class BaiduHttpTest {
-    public static void main(String[] args) throws IOException {
-        BaiduHttp.instance.orc("1","2","3").execute();
-    }
-}
