d6790632e189c733015779aca4f399d3908f8074
baichangda <471267877@qq.com>
2021-12-15 16:42:43 +0800

    c

--- src/main/java/com/bcd/base/support_baidu/BaiduConfig.java
+++ src/main/java/com/bcd/base/support_baidu/BaiduConfig.java
@@ -8,18 +8,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BaiduConfig {
 
-    @Value("${baidu.apiKey}")
-    String apiKey;
+    @Value("${baidu.secretId}")
+    String secretId;
     @Value("${baidu.secretKey}")
     String secretKey;
 
     @Bean
     public BaiduInstance baiduInstance() {
-        return BaiduInstance.newInstance(apiKey, secretKey);
+        return BaiduInstance.newInstance(secretId, secretKey);
     }
 
     @Bean
     public AipOcr aipOcr() {
-        return new AipOcr("test",apiKey, secretKey);
+        return new AipOcr("test", secretId, secretKey);
     }
 }
