544e8dc49e975b7ba819b2df8f898090118b72e1
baichangda <471267877@qq.com>
2022-01-04 14:19:17 +0800

    c

--- startBootJar.sh
+++ startBootJar.sh
@@ -1,5 +1,6 @@
 app=msbf
 pid=$(ps -ef | grep $app | grep -v grep | awk '{print $2}')
+logFilePath=logs/data.log
 if [ $pid ]; then
   echo :App is running pid=$pid
   kill -9 $pid
@@ -7,4 +8,4 @@ fi
 gradle bootJar
 echo :bootjar succeed
 nohup java -jar $app >/dev/null 2>&1 &
-echo :start succeed
+tail -f $logFilePath
