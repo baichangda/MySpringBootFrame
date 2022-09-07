d8658415beed323ae7ede7fd2da46cd703594938
baichangda <471267877@qq.com>
2022-09-07 18:13:08 +0800

    c

---
+++ src/main/java/com/bcd/base/util/FileUtil.java
@@ -0,0 +1,58 @@
+package com.bcd.base.util;
+
+
+import com.bcd.base.exception.BaseRuntimeException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileUtil {
+    /**
+     * 删除文件
+     * 或者递归删除文件夹
+     *
+     * @param path
+     */
+    public static void deleteDirRecursion(Path path) {
+        if (Files.notExists(path) || !Files.isDirectory(path)) {
+            return;
+        }
+        clearDirRecursion(path);
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException ex) {
+            throw BaseRuntimeException.getException(ex);
+        }
+    }
+
+    public static void clearDirRecursion(Path path) {
+        if (Files.notExists(path) || !Files.isDirectory(path)) {
+            return;
+        }
+        try (final Stream<Path> stream = Files.list(path)) {
+            List<Path> collect = stream.collect(Collectors.toList());
+            for (Path p : collect) {
+                try {
+                    if (Files.isDirectory(p)) {
+                        clearDirRecursion(p);
+                    }
+                    Files.delete(p);
+                } catch (IOException ex) {
+                    throw BaseRuntimeException.getException(ex);
+                }
+            }
+        } catch (IOException ex) {
+            throw BaseRuntimeException.getException(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        clearDirRecursion(Paths.get("/Users/baichangda/Downloads/PDAA202142010000520557"));
+        deleteDirRecursion(Paths.get("/Users/baichangda/Downloads/PDAA202142010000520557"));
+    }
+}
