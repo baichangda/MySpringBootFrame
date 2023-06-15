182f3334ebc82ccc6fd003d079e0080c5f5eb146
baichangda <471267877@qq.com>
2023-06-15 13:33:58 +0800

    c

--- startBootJar.sh
+++ startBootJar.sh
@@ -1,11 +1,17 @@
 app=msbf
-pid=$(ps -ef | grep $app | grep -v grep | awk '{print $2}')
 logFilePath=logs/data.log
+pid=$(ps -ef | grep $app | grep -v grep | awk '{print $2}')
 if [ $pid ]; then
-  echo :App is running pid=$pid
+  echo --------app is running pid=$pid
   kill -9 $pid
 fi
 gradle bootJar
-echo :bootjar succeed
-nohup java -jar $app >/dev/null 2>&1 &
+echo --------bootjar succeed
+nohup java -jar build/libs/$app.jar >/dev/null 2>&1 &
+echo --------nohup jar
+while [ ! -f $logFilePath ]
+do
+  echo --------log[$logFilePath] not exist, wait 1s for tail
+  sleep 1s
+done
 tail -f $logFilePath
