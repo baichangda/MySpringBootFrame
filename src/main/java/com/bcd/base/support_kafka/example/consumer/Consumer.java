e9a3bdd20fbcb29d9505dd7eb60c25ee256cc616
baichangda <471267877@qq.com>
2023-06-26 18:20:37 +0800

    c

--- src/main/java/com/bcd/base/support_kafka/example/consumer/Consumer.java
+++ src/main/java/com/bcd/base/support_kafka/example/consumer/Consumer.java
@@ -4,13 +4,14 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 
 //@Service
 public class Consumer {
     Logger logger = LoggerFactory.getLogger(Consumer.class);
 
-    @KafkaListener(topics = "zs-feedback", groupId = "zs-device-platform1")
+    @KafkaListener(topics = "test")
     public void processMessage(ConsumerRecord<byte[], byte[]> consumerRecord) {
         logger.info("receive message key[{}] value[{}]", new String(consumerRecord.key()), new String(consumerRecord.value()));
     }
--- src/main/java/com/bcd/base/support_kafka/example/producer/Producer.java
+++ src/main/java/com/bcd/base/support_kafka/example/producer/Producer.java
@@ -1,25 +1,33 @@
 package com.bcd.base.support_kafka.example.producer;
 
+import com.bcd.base.util.DateZoneUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 //@Service
 @SuppressWarnings("unchecked")
 public class Producer implements ApplicationListener<ContextRefreshedEvent> {
+
+    static Logger logger = LoggerFactory.getLogger(Producer.class);
+
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
     public void sendMessage(byte[] key, byte[] msg) {
-        kafkaTemplate.send("zs-feedback", key, msg).whenComplete((sendResult, throwable) -> {
+        kafkaTemplate.send("test", key, msg).whenComplete((sendResult, throwable) -> {
             if (throwable == null) {
-                System.out.println("onFailure!");
+                logger.info("send succeed");
             } else {
-                System.out.println("onSucceed!");
+                logger.error("send failed", throwable);
             }
         });
     }
@@ -27,7 +35,7 @@ public class Producer implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(() -> {
-            sendMessage("test".getBytes(), "bcd".getBytes());
+            sendMessage("test".getBytes(), DateZoneUtil.dateToString_second(new Date()).getBytes());
         }, 3, 3, TimeUnit.SECONDS);
     }
 }
