e76d4f6ebac4e9dc7483f009211796f069d74a61
baichangda <471267877@qq.com>
2023-08-15 09:10:44 +0800

    c

--- src/main/java/com/bcd/base/support_swagger/SwaggerConfig.java
+++ src/main/java/com/bcd/base/support_swagger/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.bcd.base.support_swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
