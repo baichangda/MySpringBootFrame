b7278b5d1def75e4ecca83d875defa0274fac6fd
baichangda <471267877@qq.com>
2023-09-20 12:24:47 +0800

    c

--- src/main/java/com/bcd/base/util/ExecutorUtil.java
+++ src/main/java/com/bcd/base/util/ExecutorUtil.java
@@ -3,18 +3,43 @@ package com.bcd.base.util;
 
 import com.bcd.base.exception.BaseRuntimeException;
 
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class ExecutorUtil {
 
-    public static void sleep_ms(long ms) {
+    /**
+     * 循环从队列里面拿数据、没有数据会阻塞
+     *
+     * @param queue    阻塞队列
+     * @param except   期望数量、当到达这个数量时候会调用 consumer
+     * @param callback 回调方法、如下两种情况会被调用
+     *                 1、缓存数量到达期望数量、此时数量为except
+     *                 2、当队列数量为空、即将进入阻塞前一刻、此时数量<except
+     * @param <T>
+     */
+    public static <T> void loop(BlockingQueue<T> queue, int except, Consumer<ArrayList<T>> callback) {
         try {
-            Thread.sleep(ms);
-        } catch (InterruptedException e) {
-            throw BaseRuntimeException.getException(e);
+            ArrayList<T> cache = new ArrayList<>(except);
+            while (true) {
+                T t = queue.take();
+                do {
+                    cache.add(t);
+                    if (cache.size() == except) {
+                        callback.accept(cache);
+                        cache.clear();
+                    }
+                    t = queue.poll();
+                } while (t != null);
+                callback.accept(cache);
+                cache.clear();
+            }
+        } catch (InterruptedException ex) {
+            throw BaseRuntimeException.getException(ex);
         }
     }
 
