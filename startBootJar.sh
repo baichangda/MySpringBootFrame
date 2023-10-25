909e3c22025a656789458c75adb5d12f59c087d6
baichangda <471267877@qq.com>
2023-10-25 16:44:57 +0800

    c

--- startBootJar.sh
+++ startBootJar.sh
@@ -1,13 +1,37 @@
-app=bcd
+app=dfcv-gateway
 logFilePath=logs/data.log
+
+gradle bootJar
+echo --------bootjar succeed
+
+jarFile=
+for file in ./build/libs/*
+do
+    if test -f $file
+    then
+        jarFile=$file
+    fi
+done
+echo --------fetch jar $jarFile
+
+if [ -z "$jarFile" ]
+then
+  echo --------exit when fetch jar failed
+  exit 0
+fi
+
+
+
+ln -s $jarFile $app
+echo --------link $app to $jarFile
+
 pid=$(ps -ef | grep $app | grep -v grep | awk '{print $2}')
 if [ $pid ]; then
   echo --------app is running pid=$pid
   kill -9 $pid
 fi
-gradle bootJar
-echo --------bootjar succeed
-nohup java -jar build/libs/$app.jar >/dev/null 2>&1 &
+
+nohup java -jar $app >/dev/null 2>&1 &
 echo --------nohup jar
 while [ ! -f $logFilePath ]
 do
