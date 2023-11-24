cf32cca8b5dd05abdc0b2f9e961ebef598f1a28b
baichangda <471267877@qq.com>
2023-11-24 16:00:56 +0800

    c

---
+++ src/main/java/com/bcd/base/support_jdbc/page/PageQuery.java
@@ -0,0 +1,93 @@
+package com.bcd.base.support_jdbc.page;
+
+
+import com.bcd.base.exception.BaseRuntimeException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+
+/**
+ * 用于分页查询的帮助类
+ * 可以{@link #PageQuery(String)}直接传入常规sql、会自动转换为countSql和pageSql、例如
+ * select a,b,c from t_vehicle where vin =? and vehicle_type like ? order by create_time desc
+ * 会生成
+ * SELECT COUNT(*) FROM T_VEHICLE WHERE VIN =? AND VEHICLE_TYPE LIKE ?
+ * SELECT A,B,C FROM T_VEHICLE WHERE VIN =? AND VEHICLE_TYPE LIKE ? ORDER BY CREATE_TIME DESC LIMIT ?,?
+ * 转换逻辑参考方法{@link #toCountSql(String)}、{@link #toPageSql(String)}
+ * sql必须满足如下条件
+ * 1、格式必须为 select ... from ... order by ...
+ * 2、order by语句可以没有
+ * 3、不能以limit语句结尾
+ *
+ * 也可以{@link #PageQuery(String, String)}自己传入countSql和pageSql
+ */
+public final class PageQuery {
+    public final String countSql;
+    public final String pageSql;
+
+    public PageQuery(String countSql, String pageSql) {
+        this.countSql = countSql;
+        this.pageSql = pageSql;
+    }
+
+    public PageQuery(String sql) {
+        this.countSql = toCountSql(sql);
+        this.pageSql = toPageSql(sql);
+    }
+
+    /**
+     * 直接在sql结尾加上limit语句
+     * @param sql
+     * @return
+     */
+    public static String toPageSql(String sql) {
+        return sql.toUpperCase().trim() + " LIMIT ?,?";
+    }
+
+    /**
+     * 寻找第一个select、from、将中间的语句替换为count(*)
+     * 寻找最后一个order by、如果存在、则去掉order by及其后面的语句
+     * @param sql
+     * @return
+     */
+    public static String toCountSql(String sql) {
+        String upperCase = sql.toUpperCase().trim();
+        int i1 = upperCase.indexOf("SELECT");
+        int i2 = upperCase.indexOf("FROM");
+        if (i1 == -1 || i2 == -1) {
+            throw BaseRuntimeException.getException("toCountSql sql[{}] not support", sql);
+        }
+        int i3 = upperCase.lastIndexOf("ORDER BY");
+        return upperCase.substring(0, i1 + 6) +
+                " COUNT(*) " +
+                (i3 == -1 ? upperCase.substring(i2) : upperCase.substring(i2, i3));
+    }
+
+
+    public <T> Page<T> query(JdbcTemplate jdbcTemplate, int pageNum, int pageSize, Class<T> clazz, Object... params) {
+        PageRequest pageRequest = PageRequest.of(pageNum, pageSize);
+        Integer total = jdbcTemplate.queryForObject(countSql, Integer.class, params);
+        if (total == null || total == 0) {
+            return Page.empty(pageRequest);
+        } else {
+            Object[] pageParams = new Object[params.length + 2];
+            System.arraycopy(params, 0, pageParams, 0, params.length);
+            pageParams[pageParams.length - 2] = pageNum * pageSize;
+            pageParams[pageParams.length - 1] = pageSize;
+            List<T> content = jdbcTemplate.query(pageSql, new BeanPropertyRowMapper<>(clazz), pageParams);
+            return new PageImpl<>(content, pageRequest, total);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        PageQuery pageQuery = new PageQuery("select a,b,c from t_vehicle where vin =? and vehicle_type like ? order by create_time desc");
+        System.out.println(pageQuery.countSql);
+        System.out.println(pageQuery.pageSql);
+    }
+}
