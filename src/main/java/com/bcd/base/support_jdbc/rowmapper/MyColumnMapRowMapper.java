ee27f9de3b4e62e81a67766b3875dc6a9dcbd806
baichangda <471267877@qq.com>
2023-12-26 10:30:23 +0800

    c

--- src/main/java/com/bcd/base/support_jdbc/rowmapper/MyColumnMapRowMapper.java
+++ src/main/java/com/bcd/base/support_jdbc/rowmapper/MyColumnMapRowMapper.java
@@ -1,13 +1,14 @@
 package com.bcd.base.support_jdbc.rowmapper;
 
 import com.bcd.base.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 
 public class MyColumnMapRowMapper extends ColumnMapRowMapper {
     public final static MyColumnMapRowMapper ROW_MAPPER = new MyColumnMapRowMapper();
 
     @Override
-    protected String getColumnKey(String columnName) {
+    protected String getColumnKey(@NotNull String columnName) {
         return StringUtil.splitCharToCamelCase(columnName, '_');
     }
 }
