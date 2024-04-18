a86093375bccd60c321c3951d32583593bee854a
baichangda <471267877@qq.com>
2024-04-18 09:51:00 +0800

    c

--- src/main/java/com/bcd/base/support_kafka/ext/datadriven/WorkHandler.java
+++ src/main/java/com/bcd/base/support_kafka/ext/datadriven/WorkHandler.java
@@ -2,6 +2,13 @@ package com.bcd.base.support_kafka.ext.datadriven;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+/**
+ * 其中如下方法调用是由executor执行的
+ * {@link #init()}
+ * {@link #destroy()}
+ * {@link #onMessage(ConsumerRecord)}
+ * 为了保证线程安全性、建议所有异步操作交给executor执行
+ */
 public abstract class WorkHandler {
     public final String id;
     public final WorkExecutor executor;
@@ -9,6 +16,7 @@ public abstract class WorkHandler {
         this.id = id;
         this.executor = executor;
     }
+
     public abstract void onMessage(ConsumerRecord<String, byte[]> msg);
     public void init() {}
     public void destroy() {
