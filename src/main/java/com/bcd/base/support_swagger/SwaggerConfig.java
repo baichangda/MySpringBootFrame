b39d3c23635449381460b12b45a47b31c9b5bf0a
baichangda <471267877@qq.com>
2024-05-13 14:46:18 +0800

    c

--- src/main/java/com/bcd/base/support_swagger/SwaggerConfig.java
+++ src/main/java/com/bcd/base/support_swagger/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.bcd.base.support_swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +18,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(new Info().title("API")
-                        .description("bcd application")
-                        .version("v0.0.1"));
+                .info(
+                        new Info().title("API")
+                                .description("bcd application")
+                                .version("v0.0.1")
+                )
+                //全局参数设置
+//                .components(
+//                        new Components().addSecuritySchemes(tokenName, new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name(tokenName))
+//                )
+                ;
     }
 }
