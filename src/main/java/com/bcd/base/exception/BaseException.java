0f646df3f6fbacdaadeb863587d7f414747db028
baichangda <471267877@qq.com>
2024-06-18 14:17:07 +0800

    c

--- src/main/java/com/bcd/base/exception/BaseException.java
+++ src/main/java/com/bcd/base/exception/BaseException.java
@@ -1,15 +1,11 @@
 package com.bcd.base.exception;
 
-import com.bcd.base.util.ExceptionUtil;
 import com.bcd.base.util.StringUtil;
 
 /**
  * 建造此异常类的目的:
  * 1、在所有需要抛非运行时异常的地方,用此异常包装,避免方法调用时候需要捕获异常(若是其他框架自定义的异常,请不要用此类包装)
  * 2、在业务需要出异常的时候,定义异常并且抛出
- * <p>
- * 注意:
- * 如果是用作第一种用途,则所有继承自Throwable的方法都是针对解析出来的真实异常,解析规则参考 {@link ExceptionUtil#parseException}
  */
 public class BaseException extends RuntimeException {
     public int code = 1;
