922963e25ab8d8d387cf3977b2ad6406b00ee00f
baichangda <471267877@qq.com>
2024-10-29 11:00:55 +0800

    c

--- src/main/java/com/bcd/base/condition/Condition.java
+++ src/main/java/com/bcd/base/condition/Condition.java
@@ -13,20 +13,20 @@ import java.util.stream.Collectors;
  * Created by Administrator on 2017/4/11.
  */
 public interface Condition extends Serializable {
-    static Condition and(List<Condition> conditionList) {
-        return new ConcatCondition(ConcatCondition.ConcatWay.AND, conditionList.stream().filter(Objects::nonNull).collect(Collectors.toList()));
+    static Condition and(List<Condition> conditions) {
+        return new ConcatCondition(ConcatCondition.ConcatWay.AND, conditions.stream().filter(Objects::nonNull).collect(Collectors.toList()));
     }
 
-    static Condition and(Condition... conditionArr) {
-        return and(Arrays.asList(conditionArr));
+    static Condition and(Condition... conditions) {
+        return and(Arrays.asList(conditions));
     }
 
-    static Condition or(List<Condition> conditionList) {
-        return new ConcatCondition(ConcatCondition.ConcatWay.OR, conditionList.stream().filter(Objects::nonNull).collect(Collectors.toList()));
+    static Condition or(List<Condition> conditions) {
+        return new ConcatCondition(ConcatCondition.ConcatWay.OR, conditions.stream().filter(Objects::nonNull).collect(Collectors.toList()));
     }
 
-    static Condition or(Condition... conditionArr) {
-        return or(Arrays.asList(conditionArr));
+    static Condition or(Condition... conditions) {
+        return or(Arrays.asList(conditions));
     }
 
     String toAnalysis();
