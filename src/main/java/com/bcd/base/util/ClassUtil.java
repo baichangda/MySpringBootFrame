befb1a7699eb75f0078d735acb869784f197ab8e
baichangda <471267877@qq.com>
2024-11-06 10:59:16 +0800

    c

--- src/main/java/com/bcd/base/util/ClassUtil.java
+++ src/main/java/com/bcd/base/util/ClassUtil.java
@@ -21,6 +21,7 @@ import java.util.stream.Collectors;
 public class ClassUtil {
     /**
      * 从指定类开始向上找，直到指定类结束
+     *
      * @param startClass
      * @param endClasses
      * @return
@@ -99,8 +100,6 @@ public class ClassUtil {
     public static List<Class<?>> getClasses(String packageName) throws IOException, ClassNotFoundException {
         // 第一个class类的集合
         List<Class<?>> classes = new ArrayList<>();
-        // 是否循环迭代
-        boolean recursive = true;
         // 获取包的名字 并进行替换
         String packageDirName = packageName.replace('.', '/');
         // 定义一个枚举的集合 并进行循环来处理这个目录下的things
@@ -117,7 +116,7 @@ public class ClassUtil {
                 // 获取包的物理路径
                 String filePath = URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8);
                 // 以文件的方式扫描整个包下的文件 并添加到集合中
-                findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
+                findAndAddClassesInPackageByFile(packageName, filePath, classes);
             } else if ("jar".equals(protocol)) {
                 // 如果是jar包文件
                 // 定义一个JarFile
@@ -141,9 +140,6 @@ public class ClassUtil {
                         if (idx != -1) {
                             // 获取包名 把"/"替换成"."
                             packageName = name.substring(0, idx).replace('/', '.');
-                        }
-                        // 如果可以迭代下去 并且是一个包
-                        if ((idx != -1) || recursive) {
                             // 如果是一个.class文件 而且不是目录
                             if (name.endsWith(".class") && !entry.isDirectory()) {
                                 // 去掉后面的".class" 获取真正的类名
@@ -164,10 +160,9 @@ public class ClassUtil {
      *
      * @param packageName
      * @param packagePath
-     * @param recursive
      * @param classes
      */
-    private static void findAndAddClassesInPackageByFile(String packageName, String packagePath, final boolean recursive, List<Class<?>> classes) throws ClassNotFoundException {
+    private static void findAndAddClassesInPackageByFile(String packageName, String packagePath, List<Class<?>> classes) throws ClassNotFoundException {
         // 获取此包的目录 建立一个File
         File dir = new File(packagePath);
         // 如果不存在或者 也不是目录就直接返回
@@ -176,12 +171,12 @@ public class ClassUtil {
         }
         // 如果存在 就获取包下的所有文件 包括目录
         // 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
-        File[] dirfiles = dir.listFiles(file -> (recursive && file.isDirectory()) || (file.getName().endsWith(".class")));
+        File[] dirs = dir.listFiles(file -> file.isDirectory() || (file.getName().endsWith(".class")));
         // 循环所有文件
-        for (File file : dirfiles) {
+        for (File file : dirs) {
             // 如果是目录 则继续扫描
             if (file.isDirectory()) {
-                findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), recursive, classes);
+                findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), classes);
             } else {
                 // 如果是java类文件 去掉后面的.class 只留下类名
                 String className = file.getName().substring(0, file.getName().length() - 6);
@@ -194,6 +189,7 @@ public class ClassUtil {
 
     /**
      * 判断类型是否是基本类型或基本类型的包装类型
+     *
      * @param clazz
      * @return
      */
